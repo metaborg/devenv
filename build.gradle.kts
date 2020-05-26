21745e3ed3cc286df42fad57bdfae694b087e5d8
Gabriel Konat <gabrielkonat@gmail.com>
2020-05-26 10:12:04 +0200

    Support Spoofax Gradle nested composite builds.

--- build.gradle.kts
+++ build.gradle.kts
@@ -29,19 +29,49 @@ devenv {
   registerRepo("jenkins.pipeline")
 }
 
-try {
-  val pie = gradle.includedBuild("pie")
-  tasks.register("buildPie") {
-    dependsOn(pie.task(":buildAll"))
+tasksWithIncludedBuild("spoofax.gradle") { spoofaxGradle ->
+  tasksWithIncludedBuild("spoofax.gradle.example") { spoofaxGradleExample ->
+    register("buildSpoofaxGradle") {
+      group = "development"
+      dependsOn(spoofaxGradle.task(":buildAll"))
+      dependsOn(spoofaxGradleExample.task(":buildAll"))
+    }
   }
-} catch(e: UnknownDomainObjectException) {
-  // Ignore
 }
 
-tasks.register("runSdf3Eclipse") {
-  dependsOn(gradle.includedBuild("spoofax.example").task(":sdf3.eclipse:run"))
-}
-tasks.register("testSdf3Spoofax") {
-  dependsOn(gradle.includedBuild("spoofax.example").task(":sdf3.spoofax:test"))
+tasksWithIncludedBuild("pie") {
+  registerDelegateTask("buildPie", it, ":buildAll")
 }
 
+tasksWithIncludedBuild("spoofax.example") {
+  registerDelegateTask("buildSpoofaxExample", it, ":buildAll")
+
+  registerDelegateTask("testMod", it, ":mod:test")
+  registerDelegateTask("testModSpoofax", it, ":mod.spoofax:test")
+  registerDelegateTask("runModCli", it, ":mod.cli:run")
+  registerDelegateTask("runModEclipse", it, ":mod.eclipse:run")
+  registerDelegateTask("runModIntelliJ", it, ":mod.intellij:runIde")
+
+  registerDelegateTask("testSdf3", it, ":sdf3:test")
+  registerDelegateTask("testSdf3Spoofax", it, ":sdf3.spoofax:test")
+  registerDelegateTask("runSdf3Cli", it, ":sdf3.cli:run")
+  registerDelegateTask("runSdf3Eclipse", it, ":sdf3.eclipse:run")
+  registerDelegateTask("runSdf3IntelliJ", it, ":sdf3.intellij:runIde")
+}
+
+fun Project.tasksWithIncludedBuild(name: String, fn: TaskContainer.(IncludedBuild) -> Unit) {
+  try {
+    tasks.fn(gradle.includedBuild(name))
+  } catch(e: UnknownDomainObjectException) {
+    // Ignore
+  }
+}
+
+fun TaskContainer.registerDelegateTask(name: String, build: IncludedBuild, vararg taskPaths: String) {
+  this.register(name) {
+    group = "development"
+    taskPaths.forEach {
+      dependsOn(build.task(it))
+    }
+  }
+}
--- settings.gradle.kts
+++ settings.gradle.kts
@@ -17,7 +17,7 @@ buildscript {
     maven("https://artifacts.metaborg.org/content/groups/public/")
   }
   dependencies {
-    classpath("org.metaborg:gradle.config:0.3.20")
+    classpath("org.metaborg:gradle.config:0.3.21")
   }
 }
 apply(plugin = "org.metaborg.gradle.config.devenv-settings")
@@ -25,8 +25,12 @@ apply(plugin = "org.metaborg.gradle.config.devenv-settings")
 // Include builds from subdirectories, but only if it is from an included repository.
 configure<DevenvSettingsExtension> {
   includeBuildsFromSubDirs(true)
+  // Manually include nested composite builds, as IntelliJ does not support them.
+  if(repoProperties["spoofax.gradle"]?.include == true && rootDir.resolve("spoofax.gradle").exists()) {
+    includeBuild("spoofax.gradle/plugin")
+    includeBuild("spoofax.gradle/example")
+  }
   if(repoProperties["spoofax-pie"]?.include == true && rootDir.resolve("spoofax.pie").exists()) {
-    // Manually include Spoofax-PIE (nested) composite builds, as IntelliJ does not support nested composite builds.
     includeBuild("spoofax.pie/core")
     includeBuild("spoofax.pie/example")
   }
