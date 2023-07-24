645c6b9accefcc603a9cdb4bd4a5f7b3d870f1bf
Daniel A. A. Pelsmaeker <647530+Virtlink@users.noreply.github.com>
2023-07-24 17:41:16 +0000

    Update gradle.config plugin for ./repo

--- repo.gradle.kts
+++ repo.gradle.kts
@@ -3,7 +3,7 @@ buildscript {
     maven("https://artifacts.metaborg.org/content/groups/public/")
   }
   dependencies {
-    classpath("org.metaborg:gradle.config:0.4.7")
+    classpath("org.metaborg:gradle.config:0.4.8")
   }
 }
 apply(plugin = "org.metaborg.gradle.config.devenv-repositories")
