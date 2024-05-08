452c215efb4e4062713fc20b8da2a6036b99d64e
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-05-08 14:18:34 +0200

    Also support Develocity plugin

--- repo.gradle.kts
+++ repo.gradle.kts
@@ -10,6 +10,13 @@ apply(plugin = "org.metaborg.gradle.config.devenv-repositories")
 
 // Auto-accept build scan TOS
 extensions.findByName("buildScan")?.withGroovyBuilder {
-  setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
-  setProperty("termsOfServiceAgree", "yes")
+  try {
+    // New Developcity plugin
+    setProperty("termsOfUseUrl", "https://gradle.com/help/legal-terms-of-use")
+    setProperty("termsOfUseAgree", "yes")
+  } catch (ex: groovy.lang.MissingPropertyException) {
+    // Deprecated Gradle Enterprise plugin
+    setProperty("termsOfServiceUrl", "https://gradle.com/terms-of-service")
+    setProperty("termsOfServiceAgree", "yes")
+  }
 }
