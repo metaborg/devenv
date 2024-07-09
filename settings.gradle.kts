b89ae540985f3507afc1cb4537dd3d7e062f3ca5
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-09 13:53:24 +0200

    Add version catalog

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,9 +1,21 @@
 rootProject.name = "devenv"
 
-// Make plugin repositories available, for loading plugins in included builds.
+// This allows us to use plugins from Metaborg Artifacts
 pluginManagement {
     repositories {
         maven("https://artifacts.metaborg.org/content/groups/public/")
+    }
+}
+
+// This allows us to use the catalog in dependencies
+dependencyResolutionManagement {
+    repositories {
+        maven("https://artifacts.metaborg.org/content/groups/public/")
+    }
+    versionCatalogs {
+        create("libs") {
+            from("org.metaborg.spoofax3:catalog:0.3.3")
+        }
     }
 }
 
