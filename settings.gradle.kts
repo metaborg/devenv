b49b5c450634d49203db2591922bdb1c552c84ff
Daniel A. A. Pelsmaeker <developer@pelsmaeker.net>
2024-07-17 17:26:24 +0200

    Update Metaborg convention

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,23 +1,5 @@
 rootProject.name = "devenv"
 
-dependencyResolutionManagement {
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-        mavenCentral()
-    }
-}
-
-pluginManagement {
-    repositories {
-        maven("https://artifacts.metaborg.org/content/groups/public/")
-        gradlePluginPortal()
-    }
-}
-
-plugins {
-    id("org.metaborg.convention.settings") version "0.8.1"
-}
-
 // Apply devenv-settings plugin. Settings plugins must still be put on the classpath via a buildscript block.
 buildscript {
     repositories {
